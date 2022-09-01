5dca973b5fb301b5fae62910a299f6e13b101014
Alexey Loiko <vincent.coffee@gmail.com>
2022-09-01 20:18:29 +0300

    Init commit

---
+++ src/main/java/com/loiko/alex/dreamcarrent/DreamCarRentApplication.java
@@ -0,0 +1,13 @@
+package com.loiko.alex.dreamcarrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamCarRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamCarRentApplication.class, args);
+    }
+
+}
